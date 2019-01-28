8cb39205c3083a43ec4126e19baf328258df61ec
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-01-28 16:00:50 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+const port  = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//   res.render('index.html');
+// });
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
