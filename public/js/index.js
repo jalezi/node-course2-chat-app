3c9a6428cdea114454d4bb2abb16ebb12e8a0126
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2019-01-29 15:06:43 +0100

    Scroll to bottom id user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,23 @@
 // request from client to server to open a websocket and keep connection open
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 // listen on event connect
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -22,6 +39,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -35,6 +53,7 @@ socket.on('newLocationMessage', function (message) {
 
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (event) {
